a352a3d88aa40fe3f1be8a73d92739da06b611b7
RrChaos <rrchaos@tutanota.com>
2019-05-15 16:23:10 +0100

    Style & finished

--- pokemon-game/src/Pokecard.js
+++ pokemon-game/src/Pokecard.js
@@ -11,7 +11,9 @@ export default class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={pokeImg} alt={this.props.name}/>
+        <div className="Pokecard-image">
+          <img src={pokeImg} alt={this.props.name} />
+        </div>
         <p className="Pokecard-data">Type: {this.props.type}</p>
         <p className="Pokecard-data">Exp: {this.props.exp}</p>  
       </div>
