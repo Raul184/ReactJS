f0ce0363cbb8603a2c1eea33d0aed0a3027aecaf
RrChaos <rrchaos@tutanota.com>
2019-05-16 16:11:58 +0100

    Dice game React

---
+++ dice-game/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import RollDice from './RollDice';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Earn 2,000 bucks for every pair of value!!!!!!! </h1>
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
---
+++ dice-game/src/Die.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Die.css';
+
+
+export default function Die() {
+  return (
+    <div className="Die">
+      <div className="Die-dice">
+        <i class="fas fa-dice-one"></i>
+      </div>
+      <div className="Die-dice">
+        <i class="`fas fa-dice-one`"></i>
+      </div>
+    </div>
+  )
+}
