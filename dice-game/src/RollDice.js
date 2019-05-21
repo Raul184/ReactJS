cfa472380954cf9bdc34f7e36cd1c041f5f54f65
RrChaos <rrchaos@tutanota.com>
2019-05-21 09:39:42 +0100

    Dice game with ReactJS

---
+++ dice-game/src/RollDice.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Die from './Die';
+import './RollDice.css';
+
+export default class RollDice extends Component {
+  static defaultProps = {
+    sides: ["one", "two", "three", "four", "five", "six"]
+  };
+  constructor(props){
+    super(props);
+    this.state = {
+      die1: 'one',
+      die2: 'one' ,
+      isRolling: false 
+    }
+  }
+  roll = () =>{
+    console.log('test');
+    // pick 2 rolls
+    let nuedie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
+    let nuedie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
+    //set state
+    this.setState({
+      die1 : nuedie1,
+      die2 : nuedie2 
+    });
+
+  }
+  render() {
+    return (
+      <div className='RollDice'>
+        <div className='RollDice-can'>
+          <Die face={this.state.die1} />
+          <Die face={this.state.die2} />
+        </div>
+        <button className="RollDice-btn" onClick={this.roll}>
+          {this.state.isRolling ? 'Rolling...' : 'Roll Dice!'}</button>        
+      </div>
+    )
+  }
+}
