743f58b5759efc0147b81766d5d8cb3182fdf9d1
RrChaos <rrchaos@tutanota.com>
2019-05-29 12:30:01 +0100

    Yahtzee game components added

--- yahtzee-game/src/Game.js
+++ yahtzee-game/src/Game.js
@@ -11,7 +11,7 @@ class Game extends Component {
     super(props);
     this.state = {
       dice: Array.from({ length: NUM_DICE }), //[]
-      locked: Array(NUM_DICE).fill(false),  // dices unlocked
+      locked: Array(NUM_DICE).fill(false),  // dices ALL unlocked
       rollsLeft: NUM_ROLLS,
       scores: {
         ones: undefined,
@@ -31,6 +31,7 @@ class Game extends Component {
     };
     this.roll = this.roll.bind(this);
     this.doScore = this.doScore.bind(this);
+    this.toggleLocked = this.toggleLocked.bind(this);
   }
 
   roll(evt) {
@@ -48,10 +49,9 @@ class Game extends Component {
     // toggle whether idx is in locked or not
     this.setState(st => ({
       locked: [
-        ...st.locked.slice(0, idx),
-        !st.locked[idx],
-        ...st.locked.slice(idx + 1)
-        // ...st.locked.splice(idx, 1)
+        ...st.locked.slice(0, idx),   // keep these ones unlocked
+        !st.locked[idx],              // flip this one
+        ...st.locked.slice(idx + 1)   // keep these ones unlocked
       ]
     }));
   }
