db809f717684d078a825c898c1e6e80da448736e
RrChaos <rrchaos@tutanota.com>
2019-06-12 12:30:16 +0100

    copy-to-clipboard functionality added

--- colours-palete-app/src/ColorBox.js
+++ colours-palete-app/src/ColorBox.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react'
 import './ColorBox.css';
+//Clipboard Usability
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
 export default class ColorBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      copied: false
+    }
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+  //ON Change Copy Clipboard Functionality
+  changeCopyState(){
+    this.setState({
+      copied : true
+    },
+    () => {
+      setTimeout(() => this.setState({ copied : false}), 1500);
+    });
+  }
   render() {
+    //Props
     const {name , background} = this.props;
+    //Clipboard
     return (
-      <div className="ColorBox" style={{background}}>
-        <div className="ColorBox-copy">
-          <div className="ColorBox-content">
-            <span>{name}</span>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div className="ColorBox" style={{background}}>
+          {/* Scale CSS Animation on CopyCLick */}
+          <div className={`OverlayInCopy ${this.state.copied && "show"}`} style={{ background }}/>
+          <div className={`Overlay-msg ${this.state.copied && "show"}`}>
+            <h1>Copied!</h1>
+            <p>{this.props.background}</p>
           </div>
-          <button className="ColorBox-copyBtn">Copy</button>
+          {/* Scale CSS Animation on CopyCLick END*/}
+          <div className="ColorBox-copy">
+            <div className="ColorBox-content">
+              <span>{name}</span>
+            </div>
+            <button className="ColorBox-copyBtn">Copy</button>
+          </div>
+          <span className="ColorBox-more">More</span>
         </div>
-        <span className="ColorBox-more">More</span>
-      </div>
+      </CopyToClipboard>
     )
   }
 }
