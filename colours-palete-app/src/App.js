01c68d6169001905f8aef0d566a7ddc3d457a75a
RrChaos <rrchaos@tutanota.com>
2019-06-18 12:25:16 +0100

    react router added

--- colours-palete-app/src/App.js
+++ colours-palete-app/src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Palette from './Palette';
 //default colors Palette for home page
 import seedColors from './seedColors';
 //colors generator
 import {generatePalette} from "./colorHelpers";
+//ROUTER
+import { Route , Switch } from "react-router-dom";
 //id
 const uuidv4 = require('uuid/v4');
 
-function App() {
+export default class App extends Component {
+  findPalette(id){
+    return seedColors.find( function(palette){
+      return palette.id === id;
+    });
+  }
+  render(){
   return (
-    <div className="App">
-      <Palette generate ={generatePalette(seedColors[4])}/>      
-    </div>
-  );
+    <Switch>
+      <Route exact path='/' render={ () => <h1>works</h1> } />
+      <Route exact path="/palette/:id" render={
+        (routeProps) =>  
+          <div className="App">
+            <Palette generate={generatePalette(this.findPalette(routeProps.match.params.id))}/>
+          </div>                   
+        }  
+      />
+    </Switch>
+  )};
 }
 
-export default App;
